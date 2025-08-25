36c4cb65d78b6559ae6f56a9565e657e1da1ba07
darshil158 <darshilrana158@gmail.com>
2025-08-25 16:58:56 +0530

    Update 5-1.c

--- 5-1.c
+++ 5-1.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 int main() {
     int r,c,sum=0;
-    printf("Enter the no. of rows :- ");
+    printf("Enter the number of rows :- ");
     scanf("%d",&r);
-    printf("Enter the no. of columns :- ");
+    printf("Enter the number of columns :- ");
     scanf("%d",&c);
     int a[r][c];
     printf("\nEnter Elements of Array A :- \n");
@@ -21,7 +21,7 @@ int main() {
         }
         printf("\n");
     }
-    printf("\nNegative elements from array A : ");
+    printf("\nNegative Elements from array A : ");
     for(int i=0;i<r;i++) {
         for(int j=0;j<c;j++) {
             if(a[i][j]<0)   printf(" %d ",a[i][j]);
